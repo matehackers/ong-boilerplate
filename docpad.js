63c8b0d26f12ef6f4d2be7bbc113abb4856ef55b
Eduardo Bohrer <nbluisrs@gmail.com>
2013-08-03 15:20:46 -0300

    add social links and more....

--- docpad.js
+++ docpad.js
@@ -55,6 +55,240 @@ module.exports = {
         name: "Associado 1",
         position: "Presidente",
         photo: "img/profile.jpg",
+        bio:"Lorem ipsum Adipisicing et velit commodo reprehenderit nostrud deserunt mollit irure ut aliquip incididunt mollit qui minim occaecat cupidatat commodo culpa consectetur nisi do commodo proident elit in consequat velit nostrud dolor sed proident sit mollit est consectetur et velit minim qui elit velit ea ut velit aliqua esse dolore cupidatat fugiat velit qui exercitation reprehenderit cillum eiusmod est et eiusmod cillum aliquip nostrud sit aliquip commodo consectetur aliquip eu ut eu nisi ad do eu est magna minim amet amet in minim cillum magna pariatur veniam cupidatat labore ullamco enim reprehenderit dolore cillum voluptate deserunt Duis proident officia reprehenderit laboris id do consequat Ut nulla voluptate deserunt nostrud ex in ex ex qui qui proident esse laboris commodo ullamco voluptate do et cillum magna veniam enim consectetur id aliquip reprehenderit ex in ad sit eu reprehenderit sint labore in quis occaecat sit proident elit id aute voluptate fugiat incididunt non cillum commodo exercitation irure nisi ullamco nostrud sunt incididunt nostrud est aute sed in non sed exercitation ex sed sunt aute ullamco eu irure cillum fugiat Ut dolore proident non enim aute in amet labore est anim nulla ea sint labore ut sit culpa incididunt deserunt mollit mollit elit laborum commodo dolor fugiat enim ut nisi quis dolor nisi consectetur consequat sunt laborum ut dolore elit commodo dolor minim aliquip Excepteur in aute eiusmod in elit ut minim cillum dolore aliqua nisi sed Ut ad.",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
+        bio:"Bio",
+        email: "email@email.com",
+        twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
+        facebook: "fb-do-associado" //Ficando http://facebook.com/fb-do-associado
+      },
+      {
+        name: "Associado 1",
+        position: "Presidente",
+        photo: "img/profile.jpg",
         bio:"Bio",
         email: "email@email.com",
         twitter: "twitter-associado", //Ficando http://twitter.com/twitter-associado
